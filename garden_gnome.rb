a3ca90f42decadd778afd4a2175314a0ad7a2479
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-08 22:36:44 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,8 @@
 # Code your instances here
+class GardenGnome
+  def initialize (name, age, gluten_allergy, personality = evil)
+    @name = name
+    @age = age
+    @gluten_allergy = gluten_allergy
+    @personality = personality
+    
